9be90c15ade07138faab27f946cea05c451538de
Robert Johnson <robertjj@gmail.com>
2025-06-10 20:56:03 -0500

    refactor: Remove AppNotifications.swift as interstitial ad reward notification is no longer needed

--- blockbomb/Core/AppNotifications.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-/// Shared notification names used throughout the app
-extension Notification.Name {
-    /// Posted when an interstitial ad completes and rewards coins
-    static let interstitialAdRewardEarned = Notification.Name("interstitialAdRewardEarned")
-}
